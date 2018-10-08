032f2d2284953f3ffa9f65615ed1817f956559a6
vimalkrishnan <vimal_viyookaran@yahoo.co.in>
2018-10-08 10:29:33 +0530

    Parser has been added

---
+++ RuleEngineChallenge/Quartic.Engine.Business/Core/ExpressionParser.cs
@@ -0,0 +1,28 @@
+﻿
+using Quartic.Engine.Business.Enums;
+using Quartic.Engine.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quartic.Engine.Business.Core
+{
+    public static class ExpressionParser
+    {
+        //This method needs to be modified using reflector and not doing presently due to lack of time
+        public static Func<Message, bool> Parse(FilterExpression filterExpression)
+        {
+            Func<Message, bool> func = null;
+            if (filterExpression.Field == "Signal")
+            {
+
+                if (filterExpression.Operators == Operators.Equals)
+                {
+                    func = new Func<Message, bool>(e => e.Signal == filterExpression.Value);
+                }
+            }
+            return func;
+        }
+
+    }
+}
